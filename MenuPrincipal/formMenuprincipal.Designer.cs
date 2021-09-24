f8224333ddaababe36a46601bcd3e8bc68239e66
Maximiliano Ponce Marquez <62770437+maxponmar@users.noreply.github.com>
2021-09-24 05:19:24 -0700

    Menu principal

---
+++ MenuPrincipal/formMenuprincipal.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace raizesMN
+{
+    partial class formMenuprincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMenuprincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRaizes = new System.Windows.Forms.Button();
+            this.btnSEL = new System.Windows.Forms.Button();
+            this.btnInterpolacion = new System.Windows.Forms.Button();
+            this.btnIntegracion = new System.Windows.Forms.Button();
+            this.btnEqnDiff = new System.Windows.Forms.Button();
+            this.btnGraficadora = new System.Windows.Forms.Button();
+            this.btnOptimizacion = new System.Windows.Forms.Button();
+            this.btnDerivar = new System.Windows.Forms.Button();
+            this.btnTabularFx = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(557, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Métodos númericos";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(369, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Maximiliano Ponce Marquez";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(340, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Instituto Tecnológico de Nogales";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(437, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ing. Mecatrónica";
+            // 
+            // btnRaizes
+            // 
+            this.btnRaizes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaizes.Location = new System.Drawing.Point(15, 115);
+            this.btnRaizes.Name = "btnRaizes";
+            this.btnRaizes.Size = new System.Drawing.Size(172, 54);
+            this.btnRaizes.TabIndex = 2;
+            this.btnRaizes.Text = "Encontrar raíz de una función";
+            this.btnRaizes.UseVisualStyleBackColor = true;
+            this.btnRaizes.Click += new System.EventHandler(this.btnRaizes_Click);
+            // 
+            // btnSEL
+            // 
+            this.btnSEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSEL.Location = new System.Drawing.Point(193, 115);
+            this.btnSEL.Name = "btnSEL";
+            this.btnSEL.Size = new System.Drawing.Size(172, 54);
+            this.btnSEL.TabIndex = 2;
+            this.btnSEL.Text = "Resolver sistema de ecuaciones lineales";
+            this.btnSEL.UseVisualStyleBackColor = true;
+            this.btnSEL.Click += new System.EventHandler(this.btnSEL_Click);
+            // 
+            // btnInterpolacion
+            // 
+            this.btnInterpolacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInterpolacion.Location = new System.Drawing.Point(15, 175);
+            this.btnInterpolacion.Name = "btnInterpolacion";
+            this.btnInterpolacion.Size = new System.Drawing.Size(172, 54);
+            this.btnInterpolacion.TabIndex = 2;
+            this.btnInterpolacion.Text = "Ajuste de curvas e interpolacion";
+            this.btnInterpolacion.UseVisualStyleBackColor = true;
+            this.btnInterpolacion.Click += new System.EventHandler(this.btnInterpolacion_Click);
+            // 
+            // btnIntegracion
+            // 
+            this.btnIntegracion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIntegracion.Location = new System.Drawing.Point(15, 234);
+            this.btnIntegracion.Name = "btnIntegracion";
+            this.btnIntegracion.Size = new System.Drawing.Size(172, 54);
+            this.btnIntegracion.TabIndex = 2;
+            this.btnIntegracion.Text = "Integración númerica";
+            this.btnIntegracion.UseVisualStyleBackColor = true;
+            this.btnIntegracion.Click += new System.EventHandler(this.btnIntegracion_Click);
+            // 
+            // btnEqnDiff
+            // 
+            this.btnEqnDiff.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEqnDiff.Location = new System.Drawing.Point(193, 234);
+            this.btnEqnDiff.Name = "btnEqnDiff";
+            this.btnEqnDiff.Size = new System.Drawing.Size(172, 54);
+            this.btnEqnDiff.TabIndex = 2;
+            this.btnEqnDiff.Text = "Resolver ecuaciones diferenciales";
+            this.btnEqnDiff.UseVisualStyleBackColor = true;
+            this.btnEqnDiff.Click += new System.EventHandler(this.btnEqnDiff_Click);
+            // 
+            // btnGraficadora
+            // 
+            this.btnGraficadora.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGraficadora.Location = new System.Drawing.Point(371, 234);
+            this.btnGraficadora.Name = "btnGraficadora";
+            this.btnGraficadora.Size = new System.Drawing.Size(172, 26);
+            this.btnGraficadora.TabIndex = 2;
+            this.btnGraficadora.Text = "Graficar funciones";
+            this.btnGraficadora.UseVisualStyleBackColor = true;
+            this.btnGraficadora.Click += new System.EventHandler(this.btnGraficadora_Click);
+            // 
+            // btnOptimizacion
+            // 
+            this.btnOptimizacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOptimizacion.Location = new System.Drawing.Point(371, 115);
+            this.btnOptimizacion.Name = "btnOptimizacion";
+            this.btnOptimizacion.Size = new System.Drawing.Size(172, 54);
+            this.btnOptimizacion.TabIndex = 2;
+            this.btnOptimizacion.Text = "Optimización";
+            this.btnOptimizacion.UseVisualStyleBackColor = true;
+            this.btnOptimizacion.Click += new System.EventHandler(this.btnOptimizacion_Click);
+            // 
+            // btnDerivar
+            // 
+            this.btnDerivar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDerivar.Location = new System.Drawing.Point(371, 174);
+            this.btnDerivar.Name = "btnDerivar";
+            this.btnDerivar.Size = new System.Drawing.Size(172, 54);
+            this.btnDerivar.TabIndex = 2;
+            this.btnDerivar.Text = "Diferenciación númerica";
+            this.btnDerivar.UseVisualStyleBackColor = true;
+            this.btnDerivar.Click += new System.EventHandler(this.btnDerivar_Click);
+            // 
+            // btnTabularFx
+            // 
+            this.btnTabularFx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTabularFx.Location = new System.Drawing.Point(371, 262);
+            this.btnTabularFx.Name = "btnTabularFx";
+            this.btnTabularFx.Size = new System.Drawing.Size(172, 26);
+            this.btnTabularFx.TabIndex = 2;
+            this.btnTabularFx.Text = "Tabular funciones";
+            this.btnTabularFx.UseVisualStyleBackColor = true;
+            this.btnTabularFx.Click += new System.EventHandler(this.btnTabularFx_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(193, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Analísis con Fourier";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // formMenuprincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 343);
+            this.Controls.Add(this.btnTabularFx);
+            this.Controls.Add(this.btnGraficadora);
+            this.Controls.Add(this.btnEqnDiff);
+            this.Controls.Add(this.btnDerivar);
+            this.Controls.Add(this.btnIntegracion);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnInterpolacion);
+            this.Controls.Add(this.btnOptimizacion);
+            this.Controls.Add(this.btnSEL);
+            this.Controls.Add(this.btnRaizes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formMenuprincipal";
+            this.Text = "Menú principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRaizes;
+        private System.Windows.Forms.Button btnSEL;
+        private System.Windows.Forms.Button btnInterpolacion;
+        private System.Windows.Forms.Button btnIntegracion;
+        private System.Windows.Forms.Button btnEqnDiff;
+        private System.Windows.Forms.Button btnGraficadora;
+        private System.Windows.Forms.Button btnOptimizacion;
+        private System.Windows.Forms.Button btnDerivar;
+        private System.Windows.Forms.Button btnTabularFx;
+        private System.Windows.Forms.Button button1;
+    }
+}
